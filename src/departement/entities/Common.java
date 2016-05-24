8a198ee861005cffbbedf4a4f33a02c772e342f1
NullQbit <es.hamza.sup@gmail.com>
2016-05-24 23:31:13 +0100

    added common attributes

--- src/departement/entities/Common.java
+++ src/departement/entities/Common.java
@@ -1,5 +1,28 @@
 package departement.entities;
 
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class Common {
 
+	@Id
+	private Long id;
+	private String intitule;
+	
+	public Common() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getIntitule() {
+		return intitule;
+	}
+
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+	
 }
