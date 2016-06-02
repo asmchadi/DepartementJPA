e5fe3ed8c76d3f93e4a3d3a066fb1f1de15f4d75
Abdelhafid El Kadiri <el9adiri@gmail.com>
2016-06-02 14:41:43 +0100

    PFE entity annotation

--- src/com/department/entities/ProjetFinEtude.java
+++ src/com/department/entities/ProjetFinEtude.java
@@ -1,14 +1,11 @@
 package com.department.entities;
 
-import java.util.List;
-
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.department.utils.Common;
+import com.department.utils.CommonWithFile;
 import com.department.utils._TableNames;
 
 /**
@@ -17,27 +14,21 @@ import com.department.utils._TableNames;
  */
 @Entity
 @Table(name = _TableNames.ProjetFinEtude)
-public class ProjetFinEtude extends Common {
+public class ProjetFinEtude extends CommonWithFile {
 
 	private String lieu;
 	private String participants;
-
-  @ManyToMany
-  @JoinTable(
-      name = _TableNames.ProjetFinEtude + _TableNames.Enseignant,
-      joinColumns=@JoinColumn(name="PROJ_ID", referencedColumnName="ID"),
-      inverseJoinColumns=@JoinColumn(name="EMP_ID", referencedColumnName="ID"))
-	private List<Enseignant> encadrants;
-	private Integer nbHeureEncadre;
-
-	public Integer getNbHeureEncadre() {
-		return nbHeureEncadre;
-	}
-
-	public void setNbHeureEncadre(Integer nbHeureEncadre) {
-		this.nbHeureEncadre = nbHeureEncadre;
-	}
-
+	private String anneeUniversitaire;
+	@ManyToOne
+	private Filiere filiere;
+	@ManyToOne
+	private Enseignant encadrants;
+	
+	@Lob
+	protected byte[] pageGarde_data;
+	protected String pageGarde_name;
+	protected String pageGarde_contentType;	
+	
 	public ProjetFinEtude() {
 		super();
 	}
@@ -50,7 +41,6 @@ public class ProjetFinEtude extends Common {
 		this.lieu = lieu;
 	}
 
-
 	public String getParticipants() {
 		return participants;
 	}
@@ -59,12 +49,54 @@ public class ProjetFinEtude extends Common {
 		this.participants = participants;
 	}
 
-	public List<Enseignant> getEncadrants() {
+	public String getAnneeUniversitaire() {
+		return anneeUniversitaire;
+	}
+
+	public void setAnneeUniversitaire(String anneeUniversitaire) {
+		this.anneeUniversitaire = anneeUniversitaire;
+	}
+
+	public Filiere getFiliere() {
+		return filiere;
+	}
+
+	public void setFiliere(Filiere filiere) {
+		this.filiere = filiere;
+	}
+
+	public Enseignant getEncadrants() {
 		return encadrants;
 	}
 
-	public void setEncadrants(List<Enseignant> encadrants) {
+	public void setEncadrants(Enseignant encadrants) {
 		this.encadrants = encadrants;
 	}
+
+	public byte[] getPageGarde_data() {
+		return pageGarde_data;
+	}
+
+	public void setPageGarde_data(byte[] pageGarde_data) {
+		this.pageGarde_data = pageGarde_data;
+	}
+
+	public String getPageGarde_name() {
+		return pageGarde_name;
+	}
+
+	public void setPageGarde_name(String pageGarde_name) {
+		this.pageGarde_name = pageGarde_name;
+	}
+
+	public String getPageGarde_contentType() {
+		return pageGarde_contentType;
+	}
+
+	public void setPageGarde_contentType(String pageGarde_contentType) {
+		this.pageGarde_contentType = pageGarde_contentType;
+	}
+
+
 
 }
